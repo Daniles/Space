31da25610dc3ca262dabb240910c9f7fbfbf4915
Daniles <danilmm@mail.ru>
2016-10-13 19:11:05 +0300

    Добавил заграждения

---
+++ Space/Assets/Scripts/DefenseHelper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenseHelper : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D colliderEnter)
+    {
+        if (colliderEnter.gameObject.GetComponent<BallController>())
+        {
+            Destroy(colliderEnter.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Space/Assets/Scripts/DefensesSpawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefensesSpawn : MonoBehaviour {
+
+    public float squareSize = 0.3f;
+    public GameObject defensePrefab;
+	// Use this for initialization
+	void Start () {
+	    for(int i = 1; defensePrefab; i++)
+        {
+            Transform defense = transform.FindChild("Defense" + i.ToString());
+            if (!defense) break;
+            for (int j = -6; j < 6; j++)
+            {
+                Instantiate(defensePrefab, new Vector3(defense.position.x + squareSize * j, defense.position.y + squareSize/2, defense.position.z), Quaternion.identity);
+            }
+            for (int j = -6; j < 6; j++)
+            {
+                (Instantiate(defensePrefab, new Vector3(defense.position.x + squareSize * j, defense.position.y - squareSize/2, defense.position.z), Quaternion.identity) as GameObject).transform.SetParent(defense);
+            }
+        }
+	}
+
+}
